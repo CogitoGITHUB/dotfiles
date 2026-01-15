3806b2f36c049eefc9ea725c11b29d344a5ddb94
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-01-15 10:11:33 +0200

    deleted:    Shapeless-OS 	deleted:    ShapelessOS package manager.org 	deleted:    ShapelessOS.org 	deleted:    test_literate.el 	deleted:    test_loader.el 	deleted:    test_single.el

--- test_literate.el
+++
@@ -1,3 +0,0 @@
-;; Test loading literate config
-(setq debug-on-error t)
-(load "/home/asdf/.config/emacs/lisp/literate-config-emacs/literate-config-loader.el")
--- test_single.el
+++
@@ -1,5 +0,0 @@
-(setq debug-on-error t
-      debug-on-quit t)
-(message "Starting test...")
-(load "/home/asdf/.config/emacs/lisp/literate-config-emacs/literate-config-emacs.el")
-(message "Loaded literate-config-emacs.el successfully")
