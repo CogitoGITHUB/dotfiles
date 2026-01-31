3be56908778f3f47fa44a2862505b6b0c0cd2687
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-01-31 03:47:28 +0200

    Fix prometheus exporter conflict

--- ShapelessOS/user-space/core/core-modules/monitoring.nix
+++ ShapelessOS/user-space/core/core-modules/monitoring.nix
@@ -1,18 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Core monitoring services
-  services.prometheus = {
-    enable = false;  # enable when ready
-  };
-
-  services.prometheus.exporters = {
-    node = {
-      enable = false;  # enable later
-      port = 9100;
-    };
-  };
-
   services.grafana = {
     enable = false;  # enable when you want dashboards
   };
@@ -26,5 +14,5 @@
   ];
 
   # Optional watchdog patterns
-  # services.systemd-watchdog.enable = true;
+   services.systemd-watchdog.enable = true;
 }
