f5f5f4fa2825dbbdd9e933eae8a142e4626325e7
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-01-31 19:41:44 +0200

    Add extreme boot debugging

--- ShapelessOS/user-space/core/core-modules/bootloader.nix
+++ ShapelessOS/user-space/core/core-modules/bootloader.nix
@@ -1,7 +1,16 @@
 { config, pkgs, ... }:
-
 {
-  # --- BOOTLOADER ------------------------------------------------------------
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Extreme boot debugging
+  boot.kernelParams = [ 
+    "boot.shell_on_fail"
+    "systemd.log_level=debug"
+    "systemd.log_target=console"
+    "rd.systemd.log_level=debug"
+  ];
+  
+  # Don't let any script kill boot
+  systemd.enableEmergencyMode = true;
 }
