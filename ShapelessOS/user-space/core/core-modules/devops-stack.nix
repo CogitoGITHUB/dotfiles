7a2f7581586c4cd3eae97ec46336fd07c28fc948
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-01-31 03:37:48 +0200

    Remove IPv6 line from PostgreSQL config

--- ShapelessOS/user-space/core/core-modules/devops-stack.nix
+++ ShapelessOS/user-space/core/core-modules/devops-stack.nix
@@ -10,7 +10,6 @@
     authentication = pkgs.lib.mkOverride 10 
       local all all trust
       host all all 127.0.0.1/32 trust
-      host all all ::1/128 trust
     ;
     initialScript = pkgs.writeText "init.sql" 
       CREATE ROLE aoeu WITH LOGIN SUPERUSER;
