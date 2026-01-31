6ee2683804e6cb2e253482f6c1ae3448300d1ac6
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-01-31 19:43:36 +0200

    Add maximum boot debugging

--- ShapelessOS/user-space/core/core-modules/bootloader.nix
+++ ShapelessOS/user-space/core/core-modules/bootloader.nix
@@ -3,14 +3,10 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   
-  # Extreme boot debugging
+  # MAXIMUM boot debugging
   boot.kernelParams = [ 
     "boot.shell_on_fail"
     "systemd.log_level=debug"
-    "systemd.log_target=console"
     "rd.systemd.log_level=debug"
   ];
-  
-  # Don't let any script kill boot
-  systemd.enableEmergencyMode = true;
 }
