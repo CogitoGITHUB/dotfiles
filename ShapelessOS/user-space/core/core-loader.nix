a4789652ddd2ee5904099fe5cdc6acf6682b1160
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-01-31 22:36:46 +0200

    Auto-commit before test

--- ShapelessOS/user-space/core/core-loader.nix
+++ ShapelessOS/user-space/core/core-loader.nix
@@ -1,30 +1,13 @@
 { lib, ... }:
 
 let
-  coreModulesDir = ./core-modules;
-  corePackagesDir = ./core-packages;
+  modulesFrom = dir:
+    lib.fileset.toList
+      (path: lib.hasSuffix ".nix" path && !(lib.hasInfix "/_" path))
+      { root = dir; };
 
-  # Recursively list .nix files
-  nixFilesRecursive = dir:
-    let
-      entries = builtins.attrNames (builtins.readDir dir);
-    in
-      lib.concatMap (name:
-        let path = "${dir}/${name}";
-            # safe directory check
-            tryDir = builtins.tryEval (builtins.readDir path);
-            isDir = tryDir.success && lib.isAttrs tryDir.value;
-        in if isDir then
-             nixFilesRecursive path
-           else if lib.hasSuffix ".nix" name then
-             [ path ]
-           else
-             []
-      ) entries;
-
-  coreModules = nixFilesRecursive coreModulesDir;
-  corePackages = nixFilesRecursive corePackagesDir;
-
+  coreModules = modulesFrom ./core-modules;
+  corePackages = modulesFrom ./core-packages;
 in
 {
   imports = coreModules ++ corePackages;
