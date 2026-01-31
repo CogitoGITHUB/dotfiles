8a3fbde18b7837eb7049277f9e049c0f37129fc1
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-02-01 01:34:05 +0200

    Clean flake with dynamic loaders

--- ShapelessOS/flake.nix
+++ ShapelessOS/flake.nix
@@ -3,17 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     scroll-flake = {
       url = "github:AsahiRocks/scroll-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     dms = {
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,34 +19,58 @@
 
   outputs = inputs@{ self, nixpkgs, home-manager, scroll-flake, dms, ... }:
   let
-    systemName = "shapeless";
+    lib = nixpkgs.lib;
+
+    findNixFiles = dir:
+      let
+        entries = builtins.readDir dir;
+        files = lib.filter (name: lib.hasSuffix ".nix" name && entries.${name} == "regular")
+          (builtins.attrNames entries);
+        dirs = lib.filter (name: entries.${name} == "directory")
+          (builtins.attrNames entries);
+      in
+        map (f: "${dir}/${f}") files
+        ++ lib.concatMap (d: findNixFiles "${dir}/${d}") dirs;
+
+    findUserFiles = dir:
+      let
+        entries = builtins.readDir dir;
+        files = lib.filter (name: lib.hasSuffix ".nix" name && entries.${name} == "regular")
+          (builtins.attrNames entries);
+        dirs = lib.filter (name: entries.${name} == "directory")
+          (builtins.attrNames entries);
+      in
+        map (f: { name = lib.removeSuffix ".nix" f; file = "${dir}/${f}"; }) files
+        ++ lib.concatMap (d: findUserFiles "${dir}/${d}") dirs;
+
+    kernelModules = findNixFiles ./kernel-space/kernel-modules;
+    coreModules = findNixFiles ./user-space/core/core-modules;
+    homeModules = findNixFiles ./user-space/home/home-modules;
+    userConfigs = builtins.listToAttrs (
+      map (u: { name = u.name; value = import u.file; }) (findUserFiles ./user-space/home/users)
+    );
   in
   {
-    nixosConfigurations.${systemName} = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.shapeless = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-
-      specialArgs = {
-        inherit inputs;
-        secretsPath = ./user-space/secrets;
-      };
-
-      modules = [
-        ./hardware-configuration.nix
-
-        ./kernel-space/kernel-loader.nix
-        ./user-space/core/core-loader.nix
-
-        scroll-flake.nixosModules.default
-        dms.nixosModules.dank-material-shell
-        home-manager.nixosModules.home-manager
-
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-        }
-
-        ./user-space/home/home-loader.nix
-      ];
+      specialArgs = { inherit inputs; };
+      modules =
+        [ ./hardware-configuration.nix ]
+        ++ kernelModules
+        ++ [
+          scroll-flake.nixosModules.default
+          dms.nixosModules.dank-material-shell
+        ]
+        ++ coreModules
+        ++ [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            imports = homeModules;
+            home-manager.users = userConfigs;
+          }
+        ];
     };
   };
 }
