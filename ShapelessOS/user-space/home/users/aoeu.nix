1803c546cfaa012c8957f80baeff50b448e69e00
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-02-01 18:46:07 +0200

    Auto-commit before aoeu

--- ShapelessOS/user-space/home/users/aoeu.nix
+++ ShapelessOS/user-space/home/users/aoeu.nix
@@ -1,10 +1,23 @@
-{ config, pkgs, ... }:
-
+{ config, pkgs, wrappers, ... }:
 {
   home.username = "aoeu";
   home.homeDirectory = "/home/aoeu";
   home.stateVersion = "24.11";
-
   programs.home-manager.enable = true;
-}
 
+  # Example: wrap mpv with config and scripts
+  home.packages = [
+    (wrappers.wrapperModules.mpv.apply {
+      inherit pkgs;
+      scripts = [ pkgs.mpvScripts.mpris ];
+      "mpv.conf".content = ''
+        vo=gpu
+        hwdec=auto
+      '';
+      "mpv.input".content = ''
+        WHEEL_UP seek 10
+        WHEEL_DOWN seek -10
+      '';
+    }).wrapper
+  ];
+}
