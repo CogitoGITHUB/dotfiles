a410f1ebac735eb951adf7b90507c8caf00a48d2
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-03-31 17:48:08 +0300

    update

--- guix/Operating-System/core-system/user-space/root/loaders/ai.scm
+++ guix/Operating-System/core-system/user-space/root/loaders/ai.scm
@@ -1,8 +1,9 @@
 (define-module (core-system user-space root loaders ai)
+  #:use-module (core-system user-space root ai openclaw)
   #:use-module (core-system user-space root ai opencode)
   #:use-module (core-system user-space root ai kilo)
-  #:re-export (opencode kilo)
+  #:re-export (openclaw opencode kilo)
   #:export (root-ai-packages))
 
 (define-public root-ai-packages
-  (list opencode kilo))
+  (list openclaw opencode kilo))
