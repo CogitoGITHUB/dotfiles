8dbaf00c4cdc19da524b407281600755ba5d9731
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-04-04 10:35:05 +0300

    update

--- guix/Operating-System/core-system/user-space/root/loaders/video.scm
+++ guix/Operating-System/core-system/user-space/root/loaders/video.scm
@@ -6,8 +6,9 @@
   #:use-module (core-system user-space root desktop video gst-editing-services)
   #:use-module (core-system user-space root desktop video mpv)
   #:use-module (core-system user-space root desktop video obs)
-  #:re-export (ffmpeg kdenlive mlt opentimelineio gst-editing-services mpv obs)
+  #:use-module (core-system user-space root desktop video xytz)
+  #:re-export (ffmpeg kdenlive mlt opentimelineio gst-editing-services mpv obs xytz)
   #:export (root-desktop-video-packages))
 
 (define-public root-desktop-video-packages
-  (list ffmpeg kdenlive mlt opentimelineio gst-editing-services mpv obs))
+  (list ffmpeg kdenlive mlt opentimelineio gst-editing-services mpv obs xytz))
