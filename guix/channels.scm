82a2c6994213a3be72d2938066cb31704f2d7db6
CogitoGITHUB <vlasceanupaulinoionut@gmail.com>
2026-04-07 14:27:58 +0300

    update

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,15 +1,9 @@
 (list (channel
   (name 'guix)
-  (url "https://git.guix.gnu.org/guix.git")
-  (branch "master"))
+  (url "file:///home/aoeu/.config/guix/sources/guix"))
       (channel
         (name 'nonguix)
-        (url "https://gitlab.com/nonguix/nonguix.git")
-        (introduction
-          (make-channel-introduction
-            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-            (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+        (url "file:///home/aoeu/.config/guix/sources/nonguix"))
       (channel
         (name 'aoeu)
-        (url "file:///home/aoeu/.config/guix"))))
+        (url "file:///home/aoeu/.config/guix")))
